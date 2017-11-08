7f1354dcbe3d6565dd3b2d2d8a4d31644f7b356a
FNGNATOR\lopez <lopezmramon@gmail.com>
2017-11-07 22:31:18 -0300

    Wallet extension added. No functionality, just tests.

---
+++ Assets/Wallet/AppliedWalletService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AppliedWalletService : MonoBehaviour
+{
+    public VerticalLayoutGroup walletDisplayLayout;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public IEnumerator CheckBlockNumber()
+    {
+        string password = "hyperbridge";
+        string path = "Assets/Hyperbridge/Resources/keystore.json";
+
+        //Read the text from directly from the test.txt file
+       /* StreamReader reader = new StreamReader(path);
+        string keystore = reader.ReadToEnd();
+        reader.Close();*/
+
+
+      //  var keyStoreService = new Nethereum.KeyStore.KeyStoreService();
+      //  byte[] key = keyStoreService.DecryptKeyStoreFromJson(password, keystore);
+       // var account = new Account(key);
+
+      //  Debug.Log(account.Address);
+       // Debug.Log(account.PrivateKey);
+
+        var wait = 1;
+        while (true)
+        {
+            yield return new WaitForSeconds(wait);
+            wait = 10;
+           // var blockNumberRequest = new EthBlockNumberUnityRequest("https://mainnet.infura.io");
+           /* yield return blockNumberRequest.SendRequest();
+            if (blockNumberRequest.Exception == null)
+            {
+                var blockNumber = blockNumberRequest.Result.Value;
+                Debug.Log("Block: " + blockNumber.ToString());
+            }*/
+        }
+    }
+
+    //TextAsset asset = Resources.Load<TextAsset>("keystore");
+
+    //try {
+    //    Debug.Log("C");
+    //    //Debug.Log(asset.text);
+    //    Debug.Log(keystore);
+    //    var password = "hyperbridge";
+    //    var account = Account.LoadFromKeyStore(keystore, password);
+    //    Debug.Log("D");
+
+    //    Nethereum.JsonRpc.Client.IClient client = new Nethereum.JsonRpc.Client.RpcClient(new Uri("https://mainnet.infura.io:443"));
+    //    var web3 = new Web3(account, "https://mainnet.infura.io:443"); // , "https://mainnet.infura.io/metamask"
+    //    Debug.Log("DD");
+    //    var accounts = await web3.Personal.ListAccounts.SendRequestAsync();
+
+    //    Debug.Log("E");
+    //    Debug.Log(string.Join(",", accounts));
+    //} catch (Exception e) {
+    //    Debug.Log(e);
+    //}
+
+    public void CreateWallet()
+    {
+        this.StartCoroutine(this.CheckBlockNumber());
+        //await web3.Personal.UnlockAccount.SendRequestAsync("0xbb7e97e5670d7475437943a1b314e661d7a9fa2a", password, new HexBigInteger(60));
+
+        //Debug.Log(web3.Personal.ListAccounts.);
+        //"0x" + EthECKey.GetPublicAddress(privateKey); //could do checksum
+        //string words = "ripple scissors kick mammal hire column oak again sun offer wealth tomorrow wagon turn fatal";
+        //string password = "TREZOR";
+        //var wallet = new Wallet(words, password);
+        //var account = wallet.GetAccount(0);
+    }
+}
+
---
+++ Assets/Wallet/WalletCreator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UMod;
+using UMod.Scripting;
+
+
+
+public class WalletCreator : MonoBehaviour {
+    string _path, receivedData;
+    public Text jsonConfirmationText;
+    public GameObject[] walletNaming;
+    public ScriptProxy proxy;
+    void Start () {
+		
+	}
+
+
+
+    public void FindJSON()
+    {
+        receivedData = proxy.Call("FindFile").ToString();
+        //JSONValidation(jsonText);
+
+    }
+
+
+    void JSONValidation(string jsonText)
+    {
+       
+        if (jsonText.Contains("address"))
+        {
+            jsonConfirmationText.text = "Valid Keystore File Loaded";
+        }
+        else
+        {
+            jsonConfirmationText.text = "Not a Valid Keystore File";
+
+        }
+    }
+
+ 
+    public void AddWallet()
+    {
+        StartCoroutine(AcceptJSON());
+    }
+    public IEnumerator AcceptJSON()
+    {
+        Debug.Log(receivedData);
+        Dictionary<string, string> values = JsonUtility.FromJson<Dictionary<string, string>>(receivedData);
+        yield return  new WaitForSeconds(1);
+
+
+
+        yield return null;
+    }
+}
